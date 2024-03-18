cc8d0a6cdb91f80b9be19029eadf17650c3fb0f7
pikadasgalaxia <cleitindopeneu@example.com>
2024-03-18 05:18:33 -0300

    criação do app e alteração na views

--- ifinder/views.py
+++ ifinder/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from utils.ifinder.factory import make_recipe
+from .models import Objeto
 
 def home(request):
     return render(request, "ifinder/pages/home.html")
 
 def lista_itens(request):
-    return render(request, "ifinder/pages/lista-itens.html", context= {'ifinder': [make_recipe() for _ in range (10)]})
+    objetos= Objeto.objects.filter(is_published=True).order_by('-id')
+    return render(request, "ifinder/pages/lista-itens.html", context= {'ifinder': objetos,})
 
 def perdi_item(request):
     return render(request, "ifinder/pages/perdi-item.html")
