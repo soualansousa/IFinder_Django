f7eb6648e71eb72f1d8eef5c443d0238410ae122
alansousa <soualansousa@gmail.com>
2024-03-18 13:29:44 -0300

    a

--- ifinder/urls.py
+++ ifinder/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
+app_name= 'ifinder'
 
 urlpatterns = [
-    path('',views.home),
+    path('',views.home ),
     path('lista-itens/',views.lista_itens),
     path('perdi-item/',views.perdi_item),
     path('encontrei-item/',views.encontrei_item),
