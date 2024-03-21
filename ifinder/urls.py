be9dbe79b67055ffe5964bcfdb3a7c56b0522876
main <gustavo.slv1516@gmail.com>
2024-03-21 10:54:56 -0300

    FINALMENTE, APARECEU OS OBJETOS

--- ifinder/urls.py
+++ ifinder/urls.py
@@ -3,8 +3,8 @@ from . import views
 app_name= 'ifinder'
 
 urlpatterns = [
-    path('',views.home),
-    path('objetos/',views.objetos),
+    path('',views.home, name= "home"),
+    path('objetos/',views.objetos, name="objetos"),
     path('perdi-objeto/',views.perdi_objeto),
     path('encontrei-objeto/',views.encontrei_objeto),
 ]
--- ifinder/views.py
+++ ifinder/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from utils.ifinder.factory import make_recipe
+from .models import Objeto
 
 def home(request):
     return render(request, "ifinder/pages/home.html")
 
 def objetos(request):
-    return render(request, "ifinder/pages/objetos.html", context= {'ifinder': [make_recipe() for _ in range (10)]})
+    #objetos= Objeto.objects.all().order_by('-id')
+    return render(request, "ifinder/pages/objetos.html", context= {'objetos':[make_recipe() for _ in range (10)]})
 
 def perdi_objeto(request):
     return render(request, "ifinder/pages/perdi-objeto.html")
