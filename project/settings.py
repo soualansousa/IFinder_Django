1090a44d9763b70bd063332fabec431e3bf7d23c
Gustavo Nascimento <gustavo.svl1516@gmail.com>
2024-04-07 15:45:28 -0300

    add pasta global static

--- project/settings.py
+++ project/settings.py
@@ -56,7 +56,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-                BASE_DIR / 'base_templates',
+            BASE_DIR / 'base_templates',
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -118,7 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'ifinder/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'base_static'
+]
 
 STATIC_ROOT = BASE_DIR / 'static'
 
