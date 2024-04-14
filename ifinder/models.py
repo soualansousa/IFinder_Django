061176d1ca6fcd8ae1eeab033ce9d68eb06f28be
alansousa <soualansousa@gmail.com>
2024-04-14 11:23:44 -0300

    Arrumando o erro do "ifinder_item_date"

--- ifinder/models.py
+++ ifinder/models.py
@@ -5,10 +5,10 @@ class Item(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
     is_published = models.BooleanField(default=False)
+    date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True
     )
-    created_at = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
         return self.title
