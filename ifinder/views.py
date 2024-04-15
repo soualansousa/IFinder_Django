1001d9a10ac03f15264bf98132212712dd91a569
Gustavo Nascimento <gustavo.svl1516@gmail.com>
2024-04-15 10:59:19 -0300

    indetação com pep8

--- ifinder/views.py
+++ ifinder/views.py
@@ -2,15 +2,19 @@ from django.shortcuts import render
 from utils.ifinder.factory import make_recipe
 from .models import Item
 
+
 def home(request):
     return render(request, "ifinder/pages/home.html")
 
+
 def lista_itens(request):
-    itens= Item.objects.all()#.order_by('-id')
-    return render(request, "ifinder/pages/lista-itens.html", context= {'itens': itens})
+    itens = Item.objects.all()  # .order_by('-id')
+    return render(request, "ifinder/pages/lista-itens.html", context={'itens': itens})
+
 
 def perdi_item(request):
     return render(request, "ifinder/pages/perdi-item.html")
+
 
 def encontrei_item(request):
     return render(request, "ifinder/pages/encontrei-item.html")
