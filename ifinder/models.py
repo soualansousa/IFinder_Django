bcc51de76e01b78b13d183e34f01cffa1f278a8c
Alan IF <alansousa456123@gmail.com>
2024-07-12 08:13:10 -0300

    Modal

--- ifinder/models.py
+++ ifinder/models.py
@@ -13,4 +13,4 @@ class Item(models.Model):
     )
 
     def __str__(self):
-        return self.title
+        return self.Título
--- ifinder/views.py
+++ ifinder/views.py
@@ -35,8 +35,8 @@ def encontrei_item(request):
     if request.method == "POST":
         lista_itens = Formulario(request.POST)
         if lista_itens.is_valid():
-            title = lista_itens.cleaned_data['title'] # noqa
-            description = lista_itens.cleaned_data['description'] # noqa
+            title = lista_itens.cleaned_data['Título'] # noqa
+            description = lista_itens.cleaned_data['Descrição'] # noqa
             itens = lista_itens.save() # noqa
             return render(request, 'ifinder/pages/cadastro-concluido.html')
     else:
