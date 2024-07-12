1cc5e79afade50f4a0e22fef70e62c4856f925a9
main <cleitindorasta.@example.com>
2024-07-12 09:03:42 -0300

    AA2

--- ifinder/urls.py
+++ ifinder/urls.py
@@ -5,15 +5,8 @@ app_name = 'ifinder'
 
 urlpatterns = [
     path('', views.home, name='home'),
-<<<<<<< HEAD
-    path('lista-itens/', views.lista_itens, name="itens"),
-    path('perdi-item/', views.perdi_item, name='perdi_item'),
-    path('encontrei-item/', views.encontrei_item, name='encontrei_item'),
-    path('login/', views.user_login, auth_views.login, name='login'),
-=======
     path('lista_itens/', views.lista_itens, name="itens"),
     path('perdi_item/', views.perdi_item, name='perdi_item'),
     path('encontrei_item/', views.encontrei_item, name='encontrei_item'),
-    path('login/', views.user_login),
->>>>>>> 6b18c12c525a36f12e5141d702f243dd933bdf12
+    path('login/', views.user_login, auth_views.login, name='login'),
 ]
