a8ddfd376ba9636cf60a7628298a93e0625b56e0
main <cleitindorasta.@example.com>
2024-07-12 09:07:04 -0300

    A

--- ifinder/urls.py
+++ ifinder/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
-from django.contrib.auth import views as auth_views
 app_name = 'ifinder'
 
 urlpatterns = [
@@ -8,5 +7,5 @@ urlpatterns = [
     path('lista_itens/', views.lista_itens, name="itens"),
     path('perdi_item/', views.perdi_item, name='perdi_item'),
     path('encontrei_item/', views.encontrei_item, name='encontrei_item'),
-    path('login/', views.user_login, auth_views.login, name='login'),
+    path('login/', views.user_login, name='login'),
 ]
