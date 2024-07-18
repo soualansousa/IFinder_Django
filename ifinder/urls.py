4dd6c85cde101a3f3c7afc701ea2bcba4f9f2ad5
main <cleitindorasta.@example.com>
2024-07-18 10:45:48 -0300

    sla oq fiz

--- ifinder/urls.py
+++ ifinder/urls.py
@@ -3,9 +3,9 @@ from . import views
 app_name = 'ifinder'
 
 urlpatterns = [
+    path('', views.user_login, name='login'),
     path('home/', views.home, name='home'),
     path('lista_itens/', views.lista_itens, name="lista_itens"),
     path('perdi_item/', views.perdi_item, name='perdi_item'),
     path('encontrei_item/', views.encontrei_item, name='encontrei_item'),
-    path('', views.login, name='login'),
 ]
