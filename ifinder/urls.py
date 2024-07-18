9fe5042bf712fda4803b535bd2cf0008aa03984f
Alan IF <alansousa456123@gmail.com>
2024-07-18 10:47:33 -0300

    .

--- ifinder/urls.py
+++ ifinder/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('lista_itens/', views.lista_itens, name="lista_itens"),
     path('perdi_item/', views.perdi_item, name='perdi_item'),
     path('encontrei_item/', views.encontrei_item, name='encontrei_item'),
-    path('', views.user_login, name='login'),
 ]
