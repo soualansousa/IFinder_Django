cec6bab6c0376f9fd37755451dcaa551368f3681
main <cleitindorasta.@example.com>
2024-07-18 10:48:45 -0300

    alteração na url

--- ifinder/urls.py
+++ ifinder/urls.py
@@ -5,7 +5,7 @@ app_name = 'ifinder'
 urlpatterns = [
     path('', views.user_login, name='login'),
     path('home/', views.home, name='home'),
-    path('lista_itens/', views.lista_itens, name="lista_itens"),
-    path('perdi_item/', views.perdi_item, name='perdi_item'),
-    path('encontrei_item/', views.encontrei_item, name='encontrei_item'),
+    path('home/lista_itens/', views.lista_itens, name="lista_itens"),
+    path('home/perdi_item/', views.perdi_item, name='perdi_item'),
+    path('home/encontrei_item/', views.encontrei_item, name='encontrei_item'),
 ]
