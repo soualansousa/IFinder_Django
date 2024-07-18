ca7dac5d54b3451097f2eb975e72f8792b260be5
Alan IF <alansousa456123@gmail.com>
2024-07-18 10:37:28 -0300

    Login

--- ifinder/urls.py
+++ ifinder/urls.py
@@ -3,9 +3,9 @@ from . import views
 app_name = 'ifinder'
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
     path('lista_itens/', views.lista_itens, name="lista_itens"),
     path('perdi_item/', views.perdi_item, name='perdi_item'),
     path('encontrei_item/', views.encontrei_item, name='encontrei_item'),
-    path('login/', views.login, name='login'),
+    path('', views.login, name='login'),
 ]
