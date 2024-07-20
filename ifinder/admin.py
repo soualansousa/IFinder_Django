308924f9fa2f7b716215b8c2a71d86df3147353e
Gusta <gustavo.slv1516@gmail.com>
2024-07-20 14:43:16 -0300

    regra de pep8 e flake8

--- ifinder/admin.py
+++ ifinder/admin.py
@@ -12,17 +12,16 @@ class ItemAdmin(admin.ModelAdmin):
 
     def mark_as_published(self, request, queryset):
         updated_count = queryset.update(Publicada=True)
-        self.message_user(request, f"{updated_count} os itens foram marcados como publicados com sucesso.")
+        self.message_user(request, f"{updated_count} os itens foram marcados como publicados com sucesso.") # noqa
 
     def mark_as_returned(self, request, queryset):
         updated_count = queryset.update(Devolvido=True, Publicada=False)
-        self.message_user(request, f"{updated_count} os itens foram marcados com sucesso como devolvidos.")
+        self.message_user(request, f"{updated_count} os itens foram marcados com sucesso como devolvidos.") # noqa
 
-    mark_as_returned.short_description = "Marcar itens selecionados como devolvidos"
+    mark_as_returned.short_description = "Marcar itens selecionados como devolvidos" # noqa
 
-    mark_as_published.short_description = "Marcar itens selecionados como publicados"
+    mark_as_published.short_description = "Marcar itens selecionados como publicados" # noqa
     actions = [mark_as_published, mark_as_returned]
-
 
 
 admin.site.register(Item, ItemAdmin)
