45955ae31aae909286fabc8d4ee5302c544f907e
Alan IF <alansousa456123@gmail.com>
2024-08-07 10:00:17 -0300

    Styles no painel do admin

--- project/settings.py
+++ project/settings.py
@@ -32,14 +32,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'jazzmin',
+    'ifinder',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ifinder',
     
 ]
 
