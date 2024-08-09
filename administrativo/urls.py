b1734a030c673d405a8b31cc9aa101439a9b87c5
Alan IF <alansousa456123@gmail.com>
2024-08-09 10:01:59 -0300

    Lista itens admin

--- administrativo/urls.py
+++ administrativo/urls.py
@@ -7,6 +7,6 @@ app_name = 'administrativo'
 urlpatterns = [
     path('', views.home, name='admin_home'),
     path('lista/', views.lista, name='lista'),
-    path('publicar-item/<int:item_id>/', views.publicar_item, name='publicar-item'),
+    path('lista/', views.publicar_item, name='publicar_item'),
     
 ]
--- administrativo/views.py
+++ administrativo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from ifinder.models import Item
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 # Create your views here.
 
@@ -11,12 +12,34 @@ def home(request):
 
 
 def lista(request):
-    itens = Item.objects.all()
-    return render(request, "administrativo/pages/lista.html", {'itens': itens})
+    status = request.GET.get('status', 'todos')
+
+    if status == 'perdido':
+        lista_itens = Item.objects.filter(Status='perdido')
+    elif status == 'encontrado':
+        lista_itens = Item.objects.filter(Status='encontrado')
+    elif status == 'devolvido':
+        lista_itens = Item.objects.filter(Status='encontrado')
+    elif status == 'publicado':
+        lista_itens = Item.objects.filter(Status='encontrado')
+    else:
+        lista_itens = Item.objects.filter()
+
+    lista_paginada = Paginator(lista_itens, 5)
+    p = request.GET.get("p")
+    try:
+        pagina = lista_paginada.page(p)
+    except PageNotAnInteger:
+        pagina = lista_paginada.page(1)
+    except EmptyPage:
+        pagina = lista_paginada.page(1)
+
+    return render(request, "administrativo/pages/lista.html", {'itens': pagina, 'status': status}) # noqa
 
 
-def publicar_item(request, item_id):
-    item = get_object_or_404(Item, id=item_id)
+def publicar_item(request):
+    item = get_object_or_404(Item)
     item.publicado = True
     item.save()
-    return redirect('lista-itens-admin')
+    return render(request, "administrativo/pages/lista.html")
+
